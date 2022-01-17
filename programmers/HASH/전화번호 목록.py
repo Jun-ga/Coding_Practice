4e636834be08512fde61febba9a8a140c006d99a
Jun-ga <6dspoo@gmail.com>
2022-01-18 00:44:34 +0900

    Update 전화번호 목록.py

--- programmers/HASH/전화번호 목록.py
+++ programmers/HASH/전화번호 목록.py
@@ -1,16 +1,6 @@
-#ver2
-def solution(phone_book):
-    answer = True
-    phone_book.sort()
-    for i in range(len(phone_book)-1):
-            if phone_book[i] == phone_book[i+1][0:len(phone_book[i])]:
-                answer = False
-
-    return answer
-
 #ver1  
 #정확도 O, 효율성 X
-'''
+
 def solution(phone_book):
     answer = True  
     temp = []
@@ -22,4 +12,17 @@ def solution(phone_book):
         for j in range(len(phone_book)-1):
             if phone_book[i] == temp[j][0:len(phone_book[i])]:
                 answer = False
-'''
+        return answer 
+
+#ver2
+#정확도 O, 효율성 O
+def solution(phone_book):
+    answer = True
+    phone_book.sort()
+    for i in range(len(phone_book)-1):
+            if phone_book[i] == phone_book[i+1][0:len(phone_book[i])]:
+                answer = False
+
+    return answer
+
+
