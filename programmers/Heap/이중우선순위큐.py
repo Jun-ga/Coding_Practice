5cccdb4c189733c506d6de1a5d190631192b1c22
Jun-ga <6dspoo@gmail.com>
2022-01-26 00:04:43 +0900

    Update 이중우선순위큐.py

--- programmers/Heap/이중우선순위큐.py
+++ programmers/Heap/이중우선순위큐.py
@@ -1,3 +1,36 @@
+import heapq
+def solution(operations):
+    answer = []
+    heap_min = [] #최소 힙
+    heap_max = [] #최대 힙
+    
+    while(operations): #연산이 남아있을때까지 동작
+        term_oper = operations.pop(0) #차례대로 연산을 oper_term 저장
+        term = int(term_oper[2:]) #연산 중 숫자만 따로 D -11 일경우 term = -11
+        if term_oper[:1] == 'I': #D -11일 경우 per_term[:1] = D 이 경우 else로 감.
+            heapq.heappush(heap_min,term) #최소힙만들기
+            heapq.heappush(heap_max,(-1 * term,term)) #최대힙 만들기, 안에 구조는 튜플구조 (예시) heap_max = [(-10,10),(12,-12)] 
+        else:
+            if len(heap_min) == 0 : pass #heap이 빈경우
+            elif term == -1: #최솟값 제거일때
+                term_min = heapq.heappop(heap_min) #최소힙에서 pop실행 이때 변수 저장해서 아래 최대힙도 pop 해야함 이유)최소힙과 최대힙은 안의 구성원이 동일해야함
+                heap_max.remove((-1 * term_min,term_min)) #최대힙은 튜플로 되어있으므로 그 형식에 맞춰서 제거
+                           
+            else: #최댓값 제거일때
+                term_max = heapq.heappop(heap_max)[1]
+                heap_min.remove(term_max)
+    
+    if heap_min :
+        answer.append(heapq.heappop(heap_max)[1])
+        answer.append(heapq.heappop(heap_min))
+    else :
+        answer.append(0)
+        answer.append(0)
+            
+    return answer
+
+#차례대로 연산을 진행하지않고 제거연산 추가연산 따로 진행하여 발생한 코드 오류
+
 '''
 import heapq
 def solution(operations):
@@ -43,33 +76,3 @@ def solution(operations):
     return answer
 
 '''
-import heapq
-def solution(operations):
-    answer = []
-    heap_min = [] #최소 힙
-    heap_max = [] #최대 힙
-    
-    while(operations): #연산이 남아있을때까지 동작
-        term_oper = operations.pop(0) #차례대로 연산을 oper_term 저장
-        term = int(term_oper[2:]) #연산 중 숫자만 따로 D -11 일경우 term = -11
-        if term_oper[:1] == 'I': #D -11일 경우 per_term[:1] = D 이 경우 else로 감.
-            heapq.heappush(heap_min,term) #최소힙만들기
-            heapq.heappush(heap_max,(-1 * term,term)) #최대힙 만들기, 안에 구조는 튜플구조 (예시) heap_max = [(-10,10),(12,-12)] 
-        else:
-            if len(heap_min) == 0 : pass #heap이 빈경우
-            elif term == -1: #최솟값 제거일때
-                term_min = heapq.heappop(heap_min) #최소힙에서 pop실행 이때 변수 저장해서 아래 최대힙도 pop 해야함 이유)최소힙과 최대힙은 안의 구성원이 동일해야함
-                heap_max.remove((-1 * term_min,term_min)) #최대힙은 튜플로 되어있으므로 그 형식에 맞춰서 제거
-                           
-            else: #최댓값 제거일때
-                term_max = heapq.heappop(heap_max)[1]
-                heap_min.remove(term_max)
-    
-    if heap_min :
-        answer.append(heapq.heappop(heap_max)[1])
-        answer.append(heapq.heappop(heap_min))
-    else :
-        answer.append(0)
-        answer.append(0)
-            
-    return answer
